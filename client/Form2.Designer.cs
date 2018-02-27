db8ee3c2bad09c4fe2eeec20969f524513d64d13
androidbigold <gx.amazing@gmail.com>
2018-02-27 13:28:40 +0800

    submit

---
+++ client/Form2.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace client
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "断开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ZC服务器IP：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ZC服务器连接";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(13, 178);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(280, 158);
+            this.panel2.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "清除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox2.Location = new System.Drawing.Point(14, 26);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 92);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "连接状态";
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(315, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(438, 150);
+            this.panel3.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox3.Location = new System.Drawing.Point(16, 16);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(409, 120);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "与ZC交互信息的状态显示";
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Controls.Add(this.textBox4);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(315, 178);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(438, 158);
+            this.panel4.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(190, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "发送";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox4.Location = new System.Drawing.Point(15, 19);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(409, 99);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "发送给ZC服务器的消息";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label8.Location = new System.Drawing.Point(267, 371);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(220, 26);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "通信1502 曲乾聪 15211041";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 406);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "车载控制器VOBC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ client/Form2.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace client
+{
+    public partial class Form2 : Form
+    {
+        //客户端与服务器之间的连接状态
+        public bool bConnected = false;
+        //监听线程
+        public Thread tAcceptMsg = null;
+        //用于socket通信的ip地址和通信端口
+        public IPEndPoint IPP = null;
+        //socket通信
+        public Socket socket = null;
+        //网络访问的基础数据流
+        public NetworkStream nStream = null;
+        //创建读取器
+        public TextReader tReader = null;
+        //创建编写器
+        public TextWriter wReader = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid);
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel2.ClientRectangle,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid);
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel3.ClientRectangle,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid);
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel4.ClientRectangle,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid,
+                Color.White, 1, ButtonBorderStyle.Solid);
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("确定要退出程序吗？", "提示", MessageBoxButtons.YesNo))
+            {
+                if (socket != null)
+                {
+                    if (socket.Connected)
+                    {
+                        MessageBox.Show("请先关闭连接");
+                        e.Cancel = true;
+                    }
+                    else
+                    {
+                        e.Cancel = false;
+                        Application.Exit();
+                    }
+                }
+                else
+                {
+                    e.Cancel = false;
+                    System.Environment.Exit(0);
+                }
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        public void AcceptMessage()
+        {
+            string sTemp;
+            while (bConnected)
+            {
+                try
+                {
+                    sTemp = tReader.ReadLine();
+                    if (sTemp.Length != 0)
+                    {
+                        lock (this)
+                        {
+                            textBox3.Text = "服务器:" + sTemp + "\r\n" + textBox3.Text;
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPP = new IPEndPoint(IPAddress.Parse(textBox1.Text), 65535);
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(IPP);
+                if (socket.Connected)
+                {
+                    nStream = new NetworkStream(socket);
+                    tReader = new StreamReader(nStream);
+                    wReader = new StreamWriter(nStream);
+                    tAcceptMsg = new Thread(new ThreadStart(this.AcceptMessage));
+                    tAcceptMsg.Start();
+                    bConnected = true;
+                    button1.Enabled = false;
+                    MessageBox.Show("与服务器成功连接，可以通信了!");
+                    textBox2.Text = DateTime.Now.ToString() + " : 与服务器" +
+                        socket.RemoteEndPoint.ToString() + "连接成功.\r\n" + textBox2.Text;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("无法与服务器通信!");
+
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (bConnected)
+            {
+                try
+                {
+                    lock (this)
+                    {
+                        textBox3.Text = "客户端:" + textBox4.Text + "\r\n" + textBox3.Text;
+                        wReader.WriteLine(textBox4.Text);
+                        wReader.Flush();
+                        textBox4.Text = "";
+                        textBox4.Focus();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("与服务器连接断开了");
+                }
+            }
+            else
+            {
+                MessageBox.Show("未与服务器建立连接，不能通信.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bConnected = false;
+            button1.Enabled = true;
+            tAcceptMsg.Abort();
+            if (socket.RemoteEndPoint != null)
+                textBox2.Text = DateTime.Now.ToString() + " : 与服务器" +
+            socket.RemoteEndPoint.ToString() + "断开连接.\r\n" + textBox2.Text;
+            if (socket.Connected)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+    }
+}
